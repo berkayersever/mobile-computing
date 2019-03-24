18199c32d9c219b6aab61b63cf25d0790f15d747
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-25 00:33:49 +0300

    Created SecondActivity.

--- layouts/app/src/main/java/com/example/layouts/MainActivity.java
+++ layouts/app/src/main/java/com/example/layouts/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.widget.Toast;
+//import android.content.res.Configuration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+//    @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//
+//        // Checks the orientation of the screen
+//        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+//            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+//        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+//            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+//        }
+//    }
 }
