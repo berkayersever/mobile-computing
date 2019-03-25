760fcee688a135207af3a0837b21bf6fc0ce2d7a
Berkay Ersever <berkayersever@sabanciuniv.edu>
2019-03-25 17:36:41 +0300

    Fixed the issues related to switching view from portrait to landscape.

--- layouts/app/src/main/java/com/example/layouts/MainActivity.java
+++ layouts/app/src/main/java/com/example/layouts/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.widget.Toast;
-//import android.content.res.Configuration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//
-//        // Checks the orientation of the screen
-//        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-//            Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
-//        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-//            Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
-//        }
-//    }
 }
